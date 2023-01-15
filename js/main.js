d469c8d484be40dc633eb66f6a67863ab6d4cb47
annvolfers <annvolfers@me.com>
2023-01-15 11:26:30 +0000

    Deploying to gh-pages from @ annvolfers/pink@3022539441d03fb8843e44ed791a5608b71df5f8 🚀

---
+++ js/main.js
@@ -0,0 +1,1 @@
+import"./mobile-menu.js";import"./map.js";import{Slider}from"./slider.js";import"./photos.js";import{AddPhoto}from"./add-photo.js";const sliders=document.querySelectorAll(".slider");sliders.forEach((o=>{new Slider(o)}));const addPhoto=document.querySelectorAll(".add-photo");addPhoto.forEach((o=>{new AddPhoto(o)}));
--- js/script.js
+++
@@ -1,1 +0,0 @@
-import"./mobile-menu.js";import"./map.js";import{Slider}from"./slider.js";const sliders=document.querySelectorAll(".slider");sliders.forEach((e=>{new Slider(e)}));
